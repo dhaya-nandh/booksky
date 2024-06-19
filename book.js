7586ed72328097483f392cf15f203c435dd281a9
Dhayanandh <159157709+dhaya-nandh@users.noreply.github.com>
2024-06-19 20:49:22 +0530

    Revert "Issues fixed"

--- book.js
+++ book.js
@@ -1,39 +1,35 @@
-var popupoverlay = document.querySelector(".popup-overlay")
-var popupbox = document.querySelector(".popup-box")
-var addpopupbutton = document.getElementById("add-popup")
+var popupoverlay=document.querySelector(".popup-overlay")
+var popupbox=document.querySelector(".popup-box")
+var addpopupbutton=document.getElementById("add-popup")
 //add button
-addpopupbutton.addEventListener("click", function () {
-    popupoverlay.style.display = "block"
-    popupbox.style.display = "block"
-})
+addpopupbutton.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupbox.style.display="block"})
 //cancel
-var cancel = document.getElementById("cancel")
+var cancel=document.getElementById("cancel")
 
-cancel.addEventListener("click", function (event) {
+cancel.addEventListener("click",function(event){
     event.preventDefault()
-    popupoverlay.style.display = "none"
-    popupbox.style.display = "none"
-})
+popupoverlay.style.display="none"
+popupbox.style.display="none"})
 //add element
-var constainer = document.querySelector(".constainer")
-var addbook = document.getElementById("add-book")
-var booktitle = document.getElementById("book-title")
-var bookauthor = document.getElementById("book-author")
-var bookdescription = document.getElementById("book-description")
+var constainer=document.querySelector(".constainer")
+var addbook=document.getElementById("add-book")
+var booktitle=document.getElementById("book-title")
+var bookauthor=document.getElementById("book-author")
+var bookdescription=document.getElementById("book-description")
 //add element
-addbook.addEventListener("click", function (event) {
+addbook.addEventListener("click",function(event){
     event.preventDefault()
-    var div = document.createElement("div")
-    div.setAttribute("class", "book-constainer")
-    div.innerHTML = `<h3>${booktitle.value}</h3>
-<h5>${bookauthor.value}</h5>
-<p>${bookdescription.value}</p>
-<button onclick="del(event)">delete</button>`
-    constainer.appendChild(div)
-    popupoverlay.style.display = "none"
-    popupbox.style.display = "none"
-})
+var div=document.createElement("div")
+div.setAttribute("class","book-constainer")
+div.innerHTML=`<h3>${book-titleinput.value}</h3>
+<h5>${book-authorinput.value}</h5>
+<p>${book-descriptioninput.value}</p>
+<button>delete</button>`
+constainer.appendChild(div)})
 //del
-function del(event) {
+function del(event)
+{
     event.target.parentElement.remove()
 }
